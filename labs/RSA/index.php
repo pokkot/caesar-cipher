61d71751a8f28a9d8d9b743ef8c10ebdbe6fe70f
Oleg Evgrashin <o.evgrashin@gmail.com>
2019-04-11 13:01:05 +0700

    fix columns

--- labs/RSA/index.php
+++ labs/RSA/index.php
@@ -28,51 +28,53 @@
     ?>
 </div>
 
-<div id="main">
-    <form action="handler.php" method="post" class="upload-form" enctype="multipart/form-data">
-        <label for="plain-text">Enter your message:</label><br>
-        <textarea name="plain-text" id="plain-text" cols="50" rows="3"><? echo $_SESSION['rsa_input'] ?? 'Hello, world! Привет, мир!' ?></textarea>
+<div class="row">
+    <div id="main" class="column">
+        <form action="handler.php" method="post" class="upload-form" enctype="multipart/form-data">
+            <label for="plain-text">Enter your message:</label><br>
+            <textarea name="plain-text" id="plain-text" cols="50" rows="3"><? echo $_SESSION['rsa_input'] ?? 'Hello, world! Привет, мир!' ?></textarea>
 
-        <div>
-            <div style="display: inline-block">
-                <p>Prime numbers:</p>
-                <label for="p">P</label>
-                <input name="p" id="p" min="0" max="99" value="<? echo $_SESSION['p'] ?? '' ?>" disabled>
-                <label for="q">Q</label>
-                <input name="q" id="q" min="0" max="99" value="<? echo $_SESSION['q'] ?? '' ?>" disabled>
+            <div>
+                <div style="display: inline-block">
+                    <p>Prime numbers:</p>
+                    <label for="p">P</label>
+                    <input name="p" id="p" value="<? echo $_SESSION['p'] ?? '' ?>" disabled type="number">
+                    <label for="q">Q</label>
+                    <input name="q" id="q" value="<? echo $_SESSION['q'] ?? '' ?>" disabled type="number">
+                </div>
+                <div style="display: inline-block">
+                    <p>Public key:</p>
+                    <label for="n">N:</label>
+                    <input name="n" id="n" value="<? echo $_SESSION['n'] ?? '' ?>" disabled type="number">
+                    <label for="e">E:</label>
+                    <input name="e" id="e" value="<? echo $_SESSION['e'] ?? '' ?>" disabled type="number">
+                </div>
+                <div style="display: inline-block">
+                    <p>Private key:</p>
+                    <label for="n">N:</label>
+                    <input name="n" id="n" value="<? echo $_SESSION['n'] ?? '' ?>" disabled type="number">
+                    <label for="d">D:</label>
+                    <input name="d" id="d" value="<? echo $_SESSION['d'] ?? '' ?>" disabled type="number">
+                </div>
             </div>
-            <div style="display: inline-block">
-                <p>Public key:</p>
-                <label for="n">N:</label>
-                <input name="n" id="n" min="0" max="99" value="<? echo $_SESSION['n'] ?? '' ?>" disabled>
-                <label for="e">E:</label>
-                <input name="e" id="e" min="0" max="99" value="<? echo $_SESSION['e'] ?? '' ?>" disabled>
-            </div>
-            <div style="display: inline-block">
-                <p>Private key:</p>
-                <label for="n">N:</label>
-                <input name="n" id="n" min="0" max="99" value="<? echo $_SESSION['n'] ?? '' ?>" disabled>
-                <label for="d">D:</label>
-                <input name="d" id="d" min="0" max="99" value="<? echo $_SESSION['d'] ?? '' ?>" disabled>
-            </div>
+
+            <input type="submit" name="action" value="Encrypt">
+        </form>
+
+        <div id="result">
+            <label for="e">Encrypted message:</label><br>
+            <textarea name="d" id="d" cols="50" rows="3" disabled><? echo $_SESSION['rsa_enc_message'] ?? '' ?></textarea>
+
+            <label for="d">Decrypted message:</label><br>
+            <textarea name="d" id="d" cols="50" rows="3" disabled><? echo $_SESSION['rsa_dec_message'] ?? '' ?></textarea>
         </div>
-
-        <input type="submit" name="action" value="Encrypt">
-    </form>
-
-    <div id="result">
-        <label for="e">Encrypted message:</label><br>
-        <textarea name="d" id="d" cols="50" rows="3" disabled><? echo $_SESSION['rsa_enc_message'] ?? '' ?></textarea>
-
-        <label for="d">Decrypted message:</label><br>
-        <textarea name="d" id="d" cols="50" rows="3" disabled><? echo $_SESSION['rsa_dec_message'] ?? '' ?></textarea>
     </div>
-</div>
 
-<div id="side">
-    <div id="log">
-        <label for="log">Log:</label><br>
-        <textarea name="log" id="log" cols="50" rows="30"><? if ($_SESSION['rsa_log']) foreach ($_SESSION['rsa_log'] as $log) { echo $log."\n"; } ?></textarea>
+    <div id="side" class="column">
+        <div id="log">
+            <label for="log">Log:</label><br>
+            <textarea name="log" id="log" cols="50" rows="30"><? if ($_SESSION['rsa_log']) foreach ($_SESSION['rsa_log'] as $log) { echo $log."\n"; } ?></textarea>
+        </div>
     </div>
 </div>
 
